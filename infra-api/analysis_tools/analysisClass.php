0f5a62a322e675181466dc8b5c7480177e4f2ee9
ganesh7024 <ganz_gi@yahoo.com>
2020-12-28 17:38:34 +0530

    Update analysisClass.php
    Adding Projection

--- infra-api/analysis_tools/analysisClass.php
+++ infra-api/analysis_tools/analysisClass.php
@@ -9,12 +9,13 @@ class analysisManager
  public $layer_name;
  public $snap_tolerance;
  public $max_pipe_length;
-   
+ public $projection;
+
  function uploadData()
     {
 
-    
-   
+
+
 	$filename = substr($this->u_file['name'], 0, -4);
 
     $path = ROOTDIR . "/analysis_tools/uploads/";
@@ -41,36 +42,50 @@ class analysisManager
 
     if ($shp_location && $shx_location && $dbf_location && $prj_location)
     {
+		
+		$proj = $this->projection;
 
-        $cmd = '"C:\Program Files\PostgreSQL\10\bin\shp2pgsql" -s 32643 -c "' . $shp_location[0] . '" ' . $uni_name;
+        $cmd = '"E:\program files\bin\shp2pgsql" -s ' . $proj . ' -c "' . $shp_location[0] . '" ' . $uni_name;
+		
+		
 
         $queries = shell_exec($cmd);
 
         $insert_to_postgis = pg_query(DBCONNECT, $queries);
-        
+		
+		$addGeom = pg_query(DBCONNECT, "alter table $uni_name add column the_geom geometry");
+		$addGeom = pg_query(DBCONNECT, "update $uni_name set the_geom = st_transform(geom, 3857)");
+		$addGeom = pg_query(DBCONNECT, "alter table $uni_name drop column geom");
+		$addGeom = pg_query(DBCONNECT, "alter table $uni_name rename the_geom to geom");
+		$addGeom = pg_query(DBCONNECT, "select UpdateGeometrySRID('$uni_name', 'geom', 3857)"); 
+		
+       // $insert_to_postgis = pg_query(DBCONNECT, "update $uni_name set geom = st_transform()");
+
 		$this->addLayerIndex($uni_name, $this->layer_name);
 		
+		
+
 	     return array(
 			    "status" => true,
                 "upload_message" => "$filename is valid GIS Data and upload sucessfull",
                 "u_name" => $uni_name
             );
     }
-	
+
 	else {
-		
+
 		  return array(
 			    "status" => false,
                 "upload_message" => "Invalid Data- add all necessary files "
-                
+
             );
-		
+
 	}
 
     }
-	
+
     function buildModel() {
-	   
+
 	  $tableName =  $this->uploadData();
 	  $tableName =  $tableName['u_name'];
 	  $analysisTable = "build_model_$tableName";
@@ -79,15 +94,15 @@ class analysisManager
 	  $max_pipe_length = $this->max_pipe_length;
 	  $output_pipes = "pipes_$tableName";
 	  $output_junctions = "junctions_$tableName";
-	  
+
 
 	  $create_analytics_table = pg_query(DBCONNECT, "create table $analysisTable as SELECT ST_Line_Substring(the_geom, $max_pipe_length*n/length, CASE WHEN $max_pipe_length*(n+1) < length THEN $max_pipe_length*(n+1)/length ELSE 1 END) As geom FROM (SELECT ST_LineMerge((ST_Dump(ST_LineMerge(ST_Node(st_union($tableName.geom))))).geom) AS the_geom, ST_Length((ST_Dump(ST_LineMerge(ST_Node(st_union($tableName.geom))))).geom) As length FROM $tableName ) AS t CROSS JOIN generate_series(0,10000) AS n WHERE n*$max_pipe_length/length < 1;");
-	  
-	  $create_topo = pg_query(DBCONNECT, "SELECT topology.CreateTopology('$topo_name', 32643)");
+
+	  $create_topo = pg_query(DBCONNECT, "SELECT topology.CreateTopology('$topo_name', 3857)");
 	  $create_topo_geom = pg_query(DBCONNECT, "SELECT topology.AddTopoGeometryColumn('$topo_name', 'public', '$analysisTable', 'topo_geom', 'LINESTRING')");
-	  
+
 	  $setting_tolerance = pg_query(DBCONNECT, "UPDATE $analysisTable SET topo_geom = topology.toTopoGeom(geom, '$topo_name', 1, $snap_tolerance)");
-	  
+
 	  $creating_output_pipes = pg_query(DBCONNECT, "create table public.$output_pipes as select * from $topo_name.edge_data");
 	  $creating_output_junctions = pg_query(DBCONNECT, "create table public.$output_junctions as select * from $topo_name.node");
 	  $adding_length_column = pg_query(DBCONNECT, "ALTER TABLE $output_pipes ADD COLUMN length VARCHAR,ADD COLUMN graph_type VARCHAR");
@@ -96,19 +111,19 @@ class analysisManager
 	  $update_mesh = pg_query(DBCONNECT, "update $output_pipes set graph_type = 'MESHED'");
 	  $update_mesh = pg_query(DBCONNECT, "update $output_pipes set graph_type = 'BRANCHED' from ( select a.edge_id from $output_pipes as a, $output_pipes as b where St_touches(ST_EndPoint(a.geom),b.geom) group by a.edge_id HAVING COUNT(*) < 2 ) as subquery where $output_pipes.edge_id = subquery.edge_id");
 	  $update_degree = pg_query(DBCONNECT, "update $output_junctions set degree = subquery.count from ( select distinct(a.node_id), count(*) from $output_junctions as a ,$output_pipes as b where st_intersects(a.geom, b.geom) group by a.node_id) as subquery where $output_junctions.node_id = subquery.node_id");
-	  
-	  
-	  $delete_topo_query = pg_query(DBCONNECT, "SELECT topology.DropTopology('$topo_name')");
-	  $delete_topo_query = pg_query(DBCONNECT, "Drop table $analysisTable");
-	  
+
+
+	  //$delete_topo_query = pg_query(DBCONNECT, "SELECT topology.DropTopology('$topo_name')");
+	  //$delete_topo_query = pg_query(DBCONNECT, "Drop table $analysisTable");
+
 	    return array(
 			    "status" => true,
                 "upload_message" => "pipes and junctions created sucessfull",
                 "pipes_data" => $output_pipes,
                 "junctions_data" => $output_junctions
             );
-    } 
-	
+    }
+
 	function unzip_file($file, $destination){
 		$zip = new ZipArchive() ;
 		if ($zip->open($file) !== TRUE) {
@@ -118,13 +133,13 @@ class analysisManager
      	$zip->close();
         return true;
 	}
-	
-	
+
+
 	function addLayerIndex($tableName, $layerName){
 		 $uid = $this->GUID();
 		$addLayer = pg_query(DBCONNECT, "insert into layers (id, layer_name, table_name) values ('$uid', '$layerName', '$tableName')");
 	}
-	
+
     function GUID()
     {
         if (function_exists('com_create_guid') === true)
@@ -134,10 +149,10 @@ class analysisManager
 
         return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535) , mt_rand(0, 65535) , mt_rand(0, 65535) , mt_rand(16384, 20479) , mt_rand(32768, 49151) , mt_rand(0, 65535) , mt_rand(0, 65535) , mt_rand(0, 65535));
     }
-	
 
 
-    
+
+
 }
 
 ?>
