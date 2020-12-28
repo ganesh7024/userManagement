b18a96918fd63f13625d20f0f85c1c47b91374a3
ganesh7024 <ganz_gi@yahoo.com>
2020-12-28 17:37:59 +0530

    Update upload.php
    Added Projection

--- infra-api/upload.php
+++ infra-api/upload.php
@@ -5,11 +5,13 @@ header("Access-Control-Allow-Methods: POST");
 require_once ('analysis_tools/analysisClass.php');
 
 $layer_name = $_POST['layer_name'];
+$projection = $_POST['projection'];
 
 if (file_exists($_FILES['shp_import']['tmp_name']) || is_uploaded_file($_FILES['shp_import']['tmp_name']))
 {
 	$data = new analysisManager();
 	$data->layer_name =  $layer_name;
+	$data->projection =  $projection;
 	$data->u_file = $_FILES['shp_import'];
 	$messsege = $data->uploadData();
 	
